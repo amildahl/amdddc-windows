69502b7f7528ecdce80bc1d279fafa4845f7f332
Andrew Mildahl <andrew@mildahl.io>
2024-09-21 07:12:38 -0500

    Basic parameters added

--- amdddc-windows/amdddc-windows.cpp
+++ amdddc-windows/amdddc-windows.cpp
@@ -1,20 +1,113 @@
 // amdddc-windows.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <functional>
 #include <iostream>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <iomanip>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#define MAX_NUM_DISPLAY_DEVICES             16
+
+using namespace std;
+
+void vSetVcpCommand(unsigned char ucVcp, unsigned int ulVal);
+
+struct Settings {
+    bool help{ false };
+    bool verbose{ false };
+    bool detect{ false };
+    unsigned int i2c_subaddress{ 0x51 };
+    unsigned int command;
+    unsigned int input;
+    unsigned int monitor;
+    unsigned int display;
+};
+
+typedef function<void(Settings&) > ArgHandle;
+typedef function<void(Settings&, const unsigned int)> OneArgHandle;
+typedef function<void(Settings&, const unsigned int, const unsigned int)> TwoArgHandle;
+
+#define ZERO_ARGS(str, f, v) {str, [](Settings& s) {s.f = v;}}
+#define ONE_ARG(str, f, v) {str, [](Settings& s, const unsigned int arg) {s.f = v;}}
+#define TWO_ARGS(str, f1, v1, f2, v2) {str, [](Settings& s, const unsigned int arg1, const unsigned int arg2) {s.f1 = v1; s.f2 = v2;}}
+
+
+const unordered_map<string, ArgHandle> ZeroArgs{
+    ZERO_ARGS("--help", help, true),
+    ZERO_ARGS("-h",     help, true),
+
+    ZERO_ARGS("--verbose", verbose, true),
+    ZERO_ARGS("-v",        verbose, true),
+
+    ZERO_ARGS("detect", detect, true)
+};
+
+const unordered_map<string, OneArgHandle> OneArg{
+    ONE_ARG("--i2c-source-addr", i2c_subaddress, arg),
+    ONE_ARG("-m", monitor, arg),
+    ONE_ARG("-d", display, arg),
+
+};
+
+const unordered_map<string, TwoArgHandle> TwoArgs{
+    TWO_ARGS("setvcp", command, arg1, input, arg2),  
+};
+
+Settings parse_settings(int argc, const char* argv[]) {
+    Settings settings;
+
+    for (int i{ 1 }; i < argc; i++) {
+        string opt{ argv[i] };
+
+        if (auto j{ ZeroArgs.find(opt) }; j != ZeroArgs.end()) {
+            j->second(settings);
+        }
+        else if (auto k{ OneArg.find(opt) }; k != OneArg.end()) {
+            if (++i < argc) {
+                istringstream converter(argv[i]);
+                unsigned int value;
+                converter >> hex >> value;
+                k->second(settings, { value });
+            }
+            else {
+                throw runtime_error{ "missing param after " + opt };
+            }
+
+            // No, has infile been set yet?
+        }
+        else if (auto l{ TwoArgs.find(opt) }; l != TwoArgs.end()) {
+            if (++i + 1 < argc) {
+                istringstream converter1(argv[i]), converter2(argv[++i]);
+                unsigned int value1, value2;
+                converter1 >> hex >> value1;
+                converter2 >> hex >> value2;
+                l->second(settings, { value1 }, { value2 });
+            }
+            else {
+                throw runtime_error{ "missing param after " + opt };
+            }
+        }
+        else {
+            cerr << "unrecognized command-line option " << opt << endl;
+        }
+    }
+
+    return settings;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main(int argc, const char* argv[])
+{
+    Settings settings = parse_settings(argc, argv);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    if (settings.detect) {
+        cout << "Detect!";
+    }
+
+    if (settings.input != NULL) {
+        cout << "Switch! " << settings.command << " " << settings.input;
+    }
+}
